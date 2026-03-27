9ec25a3b1949961a5c7bcb98cc4c9d2028d33d0f
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2026-03-27 19:19:27 +0100

    fix(api): deactivate retry

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ const queryClient = new QueryClient({
     queries: {
       staleTime: 5 * 60 * 1000, // 5 minutes
       gcTime: 10 * 60 * 1000, // 10 minutes
+      retry: false,
     },
   },
 })
