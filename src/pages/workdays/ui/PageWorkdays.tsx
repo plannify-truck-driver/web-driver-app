d7b45997e2e4de62c18d78afb38f80891a31c81b
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2026-04-17 19:30:44 +0200

    feat: add "add workday" button

--- src/pages/workdays/ui/PageWorkdays.tsx
+++ src/pages/workdays/ui/PageWorkdays.tsx
@@ -2,9 +2,19 @@ import { PeriodSelector } from "@/shared/components/PeriodSelector"
 import { StatOvernightRest } from "@/shared/components/statistics/StatOvernightRest"
 import { StatTotalWorkedHours } from "@/shared/components/statistics/StatTotalWorkedHour"
 import { StatWorkedDays } from "@/shared/components/statistics/StatWorkedDays"
+import { Button } from "@/shared/components/ui/Button"
+import { ButtonGroup } from "@/shared/components/ui/ButtonGroup"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/shared/components/ui/DropdownMenu"
 import { WorkdayTable } from "@/shared/components/WorkdayTable"
 import { upperCaseFirstLetter } from "@/shared/functions/upperCaseFirstLetter"
 import type { Workday } from "@/shared/models/workday"
+import { ChevronDownIcon, FileChartColumnIncreasingIcon, FileCodeIcon } from "lucide-react"
 import { useTranslation } from "react-i18next"
 
 interface PageWorkdaysProps {
@@ -34,14 +44,40 @@ export default function PageWorkdays({
     <div className="flex flex-col gap-5 sm:gap-6">
       <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between sm:gap-5">
         <h1 className="text-2xl font-semibold">{t("pages.workdays.page-title")}</h1>
-        <PeriodSelector
-          label={upperCaseFirstLetter(
-            selectedMonth.toLocaleDateString(i18n.language, { month: "long", year: "numeric" })
-          )}
-          subLabel={t(selectedMonthSubTitle)}
-          onPrevious={onPreviousMonth}
-          onNext={onNextMonth}
-        />
+        <div className="flex items-center gap-4">
+          <ButtonGroup>
+            <Button variant="default" size="sm" onClick={() => window.print()}>
+              {t("pages.workdays.buttons.add-workday")}
+            </Button>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild className="h-auto">
+                <Button variant="default">
+                  <ChevronDownIcon size={16} />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end" className="w-auto max-w-70 min-w-50">
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <FileChartColumnIncreasingIcon />
+                    {t("pages.workdays.buttons.use-excel-file")}
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <FileCodeIcon />
+                    {t("pages.workdays.buttons.use-csv-file")}
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </ButtonGroup>
+          <PeriodSelector
+            label={upperCaseFirstLetter(
+              selectedMonth.toLocaleDateString(i18n.language, { month: "long", year: "numeric" })
+            )}
+            subLabel={t(selectedMonthSubTitle)}
+            onPrevious={onPreviousMonth}
+            onNext={onNextMonth}
+          />
+        </div>
       </div>
       <div className="flex w-full flex-col gap-3">
         <p className="text-muted-foreground block font-mono text-sm uppercase sm:hidden">
