eae597afc218d4bc05f2f45b5db3acbb2a192f52
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2026-05-24 18:12:48 +0200

    fix: enable skipWaiting for PWA service worker

--- vite.config.ts
+++ vite.config.ts
@@ -46,6 +46,7 @@ export default defineConfig({
         ],
       },
       workbox: {
+        skipWaiting: true,
         globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
         runtimeCaching: [
           {
